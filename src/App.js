673d97ca8bc2f22823bc03c22ac85124c89e4566
Damian Szatkowski <damianszatkowski387@gmail.com>
2020-08-23 10:36:54 +0200

    basic router and navbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {Navbar, Nav} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./components/home";
+import JokesList from "./components/jokesList";
+import JokeInfo from "./components/jokeInfo";
+import UserInfo from "./components/userInfo";
+import PageNotFound from "./components/pageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/"> JokeApp </Navbar.Brand>
+
+          <Nav className="mr-auto">
+            <Nav.Link href="/jokes"> Jokes </Nav.Link>
+          </Nav>
+        </Navbar>
+      </div>
+
+      <div>
+        <Switch>
+          <Route exact path={"/"} component={Home}/>
+          <Route exact path={"/jokes"} component={JokesList}/>
+          <Route exact path={"/jokes/:id"} component={JokeInfo}/>
+          <Route exact path={"/users/:id"} component={UserInfo}/>
+          <Route component={PageNotFound}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
