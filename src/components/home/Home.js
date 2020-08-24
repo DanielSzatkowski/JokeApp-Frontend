ec192a9409efd559e30d386a74a64abfe2d89205
Damian Szatkowski <damianszatkowski387@gmail.com>
2020-08-24 17:50:57 +0200

    comopnent Home created; prototype of main page of app created

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,17 @@
 import React, {Component} from "react";
+import "./home.css";
+import MainCard from "./mainCard";
+import AppInfo from "./appInfo";
+import Footer from "./footer"
 
 
 class Home extends Component {
     render(){
         return(
             <>
+                <MainCard/>
+                <AppInfo/>
+                <Footer/>
             </>
         )
     };
