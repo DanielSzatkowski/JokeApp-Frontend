27e4a2b53599adc359f70b7fd5731d54a2433b15
Daniel Szatkowski <danielszatkowski432@gmail.com>
2020-08-26 00:56:23 +0200

    login component looks changed; react-bootstrap added

--- src/components/forms/login/Login.js
+++ src/components/forms/login/Login.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Formik, Field, Form, ErrorMessage} from 'formik';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
 import Auth from '../../../services/authServ';
-import { Spinner } from 'react-bootstrap';
+import { Container, Row, Form, Button, Spinner } from 'react-bootstrap';
 
 class Login extends Component {
 
@@ -16,63 +16,87 @@ class Login extends Component {
 
     render() {
         return (
-            <Formik
-                initialValues = {{
-                    login: '',
-                    password: '',
-                }} 
-                validationSchema = { Yup.object({
-                    login: Yup.string()
-                        .required('Login is required!'),
-                    password: Yup.string()
-                        .required('Password is required!')
-                })}
-                onSubmit = {(values, {setSubmitting, resetForm}) => {
-                    setSubmitting(true);
-                    console.log(typeof Auth.authorize());
-
-                    Auth.authorize(values.login, values.password)
-                        .then(() => {
-                            this.props.history.push("/jokes"); //TODO: navigation
-                            window.location.reload();
-                        })
-                        .catch((err) => {
-                            this.setState({ loginFailed: true });
-                            console.log(err);
-                        });
-
-                    setSubmitting(false);
-                    resetForm();
-                }}>
+            <Container className="mt-3 p-3 fluid">
+                <Row className="mt-5 pb-2 justify-content-center text-center">
+                    <h2>LOG IN</h2>
+                </Row>
                 
-                {({ errors, touched, isSubmitting }) => (
-                    <Form>
-                        <label htmlFor="login">login</label>
-                        <Field id="login" 
-                            name="login" 
-                            placeholder="login" 
-                            disabled={isSubmitting}
-                            className={(errors.login && touched.login) ? 'is-invalid' : ''}
-                        />
-                        <ErrorMessage name="login" component="div" className="invalid-feedback" />
+                <Row className="p-2 justify-content-center text-center">
+                    <Formik
+                        initialValues = {{
+                            login: '',
+                            password: '',
+                        }} 
+                        validationSchema = { Yup.object().shape({
+                            login: Yup.string()
+                                .required('Login is required!'),
+                            password: Yup.string()
+                                .required('Password is required!')
+                        })}
+                        onSubmit = {(values, {setSubmitting, resetForm}) => {
+                            setSubmitting(true);
+                            console.log("tutaj");
 
-                        <label htmlFor="password">password</label>
-                        <Field id="password" 
-                            name="password" 
-                            placeholder="login"
-                            type="password" 
-                            disabled={isSubmitting}
-                            className={(errors.password && touched.password) ? 'is-invalid' : ''}
-                        />
-                        <ErrorMessage name="password" component="div" className="invalid-feedback" />
-                        { this.state.loginFailed ? <div> Login has failed! Please try again! </div> : null }
+                            Auth.authorize(values.login, values.password)
+                                .then(() => {
+                                    this.props.history.push("/jokes"); //TODO: navigation
+                                    window.location.reload();
+                                })
+                                .catch((err) => {
+                                    this.setState({ loginFailed: true });
+                                    console.log(err);
+                                });
 
-                        <button type="submit" disabled={isSubmitting}>
-                            {isSubmitting ? <Spinner animation="border"/> : "Submit" }
-                        </button>
-                    </Form>
-                )}
-            </Formik>
+                            setSubmitting(false);
+                            resetForm();
+                        }}>
+                        
+                        {({ values, errors, touched, isSubmitting, handleBlur, handleChange, handleSubmit }) => (
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group as={Row} className='mx-5'>
+                                    <Form.Control
+                                        type='text'
+                                        placeholder='login'
+                                        name='login'
+                                        value={values.login}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        isInvalid={touched.login && errors.login}
+                                        disabled={isSubmitting}
+                                    />  
+                                </Form.Group>
+                                {(touched.login && errors.login) && (
+                                    <Form.Control.Feedback type='invalid'>{errors.login}</Form.Control.Feedback>
+                                )}
+
+                                <Form.Group as={Row} className='mx-5'>
+                                    <Form.Control
+                                        type='password'
+                                        placeholder='password'
+                                        name='password'
+                                        value={values.password}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        isInvalid={touched.password && errors.password}
+                                        disabled={isSubmitting}
+                                    />  
+                                </Form.Group>
+                                {(touched.password && errors.password) && (
+                                    <Form.Control.Feedback type='invalid'>{errors.password}</Form.Control.Feedback>
+                                )}
+                                { this.state.loginFailed ? <div> Login has failed! Please try again! </div> : null }
+                                
+                                <Form.Group as={Row} className="d-flex justify-content-center mx-5">
+                                    <Button type="submit" disabled={isSubmitting} className="d-flex justify-content-center mx-5">
+                                        {isSubmitting ? <Spinner animation="border"/> : "Log in" }
+                                    </Button>
+                                </Form.Group>
+                            </Form>
+                        )}
+                    </Formik>
+                </Row>
+                        
+            </Container>
         );
     }
 
