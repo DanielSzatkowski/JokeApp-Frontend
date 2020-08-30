e2f977c24f6e85f52b5e73d2868b11dad60bdb95
Damian Szatkowski <damianszatkowski387@gmail.com>
2020-08-30 22:59:32 +0200

    CommentsList component added

---
+++ src/components/commentsList/CommentsList.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+
+
+class CommentsList extends Component {
+
+    constructor(props){
+        super(props);
+
+
+    }
+
+
+    render(){
+        <>
+        </>
+    }
+
+}
+
+export default CommentsList;
